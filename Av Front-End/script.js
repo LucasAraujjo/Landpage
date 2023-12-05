c906971014fc26ca5d808331497be22a4016baee
Lucas Araújo <141671473+LucasAraujjo@users.noreply.github.com>
2023-12-05 20:54:28 -0300

    Add files via upload

---
+++ Av Front-End/script.js
@@ -0,0 +1,24 @@
+let currentMemberIndex = 0;
+
+function showNextMember() {
+    currentMemberIndex = (currentMemberIndex + 2) % 5;
+    updateMembers();
+}
+
+function showPrevMember() {
+    currentMemberIndex = (currentMemberIndex - 2 + 5) % 5;
+    updateMembers();
+}
+
+function updateMembers() {
+    const membersContainer = document.getElementById('membersContainer');
+    const offset = -currentMemberIndex * (100 / 2); // Ajuste para mostrar dois membros por vez
+    membersContainer.style.transition = 'transform 0.5s ease-in-out';
+    membersContainer.style.transform = `translateX(${offset}%)`;
+}
+
+// Limpar a transição após a animação para permitir navegação contínua
+document.getElementById('membersContainer').addEventListener('transitionend', () => {
+    const membersContainer = document.getElementById('membersContainer');
+    membersContainer.style.transition = ''; // Remova o valor da propriedade transition
+});
